5216b51f300b3dba87b9ca55d9d509461881d6b3
devboki <octsixxth@gmail.com>
2021-12-22 19:01:28 +0900

    repository + service

---
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,81 @@
+package jpabook.jpashop.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+	
+	private final OrderRepository orderRepository;
+	private final MemberRepository memberRepository;
+	private final ItemRepository itemRepository;
+
+	/* 주문 */
+	@Transactional
+	public Long order(Long memberId, Long itemId, int count) {
+		
+		//엔티티 조회
+		Member member = memberRepository.findOne(memberId);
+		Item item = itemRepository.findOne(itemId);
+		
+		//배송정보 생성
+		Delivery delivery = new Delivery();
+		delivery.setAddress(member.getAddress());
+		
+		//주문상품 생성
+		OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+		/* OrderItem orderItem1 = new OrderItem(); 직접 생성 사용을 막기 위해서 
+		 * OrderItem.java -> protected OrderItem() {} or
+		 * @NoArgsConstructor(access = AccessLevel.PROTECTED) 선언*/
+		
+		//주문 생성
+		Order order = Order.createOrder(member, delivery, orderItem);
+		/* new Order(); 직접 생성 사용을 막기 위해 Order.java
+		 * @NoArgsConstructor(access = AccessLevel.PROTECTED) 선언 */
+		
+		//주문 저장
+		orderRepository.save(order);
+		
+		return order.getIdLong();
+	}
+	
+	
+	/* 취소 */
+	@Transactional
+	public void cancelOrder(Long orderId) {
+		//주문 엔티티 조회
+		Order order = orderRepository.findOne(orderId);
+		//주문 취소
+		order.cancel();
+	}
+	
+	
+	/* 검색 */
+	//211223 부터
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,12 +1,17 @@
 package jpabook.jpashop.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import jpabook.jpashop.domain.Member;
@@ -15,11 +20,15 @@ import jpabook.jpashop.repository.MemberRepository;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
+@TestPropertySource(properties = "spring.config.location=classpath:applicationTest.yml" )
 public class MemberServiceTest {
 
 	@Autowired MemberService memberService;
 	@Autowired MemberRepository memberRepository;
+	@Autowired EntityManager em;
 	
+	@Test
+	@Rollback(false)
 	public void 회원가입() throws Exception {
 		//given
 		Member member = new Member();
@@ -29,10 +38,24 @@ public class MemberServiceTest {
 		Long saveId = memberService.join(member);
 		
 		//then
+		em.flush();
 		assertEquals(member, memberRepository.findOne(saveId));
 	}
 	
+	@Test(expected = IllegalStateException.class)
 	public void 중복_회원_예외() throws Exception{
+		//given
+		Member member1 = new Member();
+		member1.setName("kim");
 		
+		Member member2 = new Member();
+		member2.setName("kim");
+		
+		//when
+		memberService.join(member1);
+		memberService.join(member2);
+		
+		//then
+		fail("예외가 발생해야 한다.");
 	}
 }
